02e7287db109f360d46114a5f7a72e89f6045841
Harendra Bhujel <avash.nekros274@gmail.com>
2021-10-22 08:25:01 +0545

    fetch hook

--- src/utilities/useFetch.js
+++ src/utilities/useFetch.js
@@ -1,1 +1,27 @@
 import React, { useEffect, useState } from 'react';
+
+const useFetch = (apiEndPoint) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchApi = async (apiEndPoint) => {
+      const response = await fetch(apiEndPoint);
+
+      if (!response.ok) {
+        const message = `An error has occured: ${response.status}`;
+        throw new Error(message);
+      }
+
+      const data = await response.json();
+      setData(data.response);
+      setIsLoading(false);
+    };
+
+    fetchApi(apiEndPoint);
+  }, []);
+
+  return [isLoading, data];
+};
+
+export default useFetch;
